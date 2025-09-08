01d69e7cf7631f74dd595243198689af193fb0b2
azurata <git@azurata.me>
2025-09-08 18:36:58 +0900

    feat(nvim): Add keybind for exit terminal

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -1,2 +1,8 @@
-vim.keymap.set({"i", "c"}, [[<C-j>]], [[<Plug>(skkeleton-enable)]], { noremap = false })
+map = vim.keymap.set
+
+-- Ctrl-jでskkeletonを有効にする
+map({"i", "c"}, [[<C-j>]], [[<Plug>(skkeleton-enable)]], { noremap = false })
+
+-- ターミナルモードでEscを2回押してノーマルモードにする
+map({"t"}, [[<Esc><Esc>]], [[<C-\><C-n>]])
 
