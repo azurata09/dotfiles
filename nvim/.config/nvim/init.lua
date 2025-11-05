f8d59290c761b2d7773a1e829b9184198cec4dac
azurata <git@azurata.me>
2025-11-05 14:07:41 +0900

    Add clipboard and neovide font settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,8 @@ require("keymap")
 
 vim.opt.number = true
 
+vim.opt.clipboard:append({"unnamedplus"})
+
+if vim.g.neovide then
+  vim.o.guifont = "HackGen Console NF:h22" -- text below applies for VimScript
+end
