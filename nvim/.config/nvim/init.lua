c1c3142136673d24667e49700c6e1d81e0a1ca4d
azurata <git@azurata.me>
2025-12-08 15:10:58 +0900

    feat(nvim): Neovideを透過させるようにした

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,5 +7,7 @@ vim.opt.clipboard:append({"unnamedplus"})
 
 if vim.g.neovide then
   vim.o.guifont = "HackGen Console NF:h22" -- text below applies for VimScript
+  vim.g.neovide_opacity = 0.75
+  vim.g.transparency = 0.75
 end
 
